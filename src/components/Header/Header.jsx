017063886db18f49bf581412a919c26e81cd5786
Kalinichenko1410 <142255469+Kalinichenko1410@users.noreply.github.com>
2024-12-24 05:07:08 +0100

    add btn logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,46 @@
 import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 import clsx from 'clsx';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn, selectUser } from '../../redux/auth/selectors';
 
 
+
+
+const Header = () => {
 const buildLinkClass = ({ isActive }) => {
   return clsx(s.links, isActive && s.activeLink);
 };
-
-const Header = () => {
+  const user = useSelector(selectUser);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
-      <div className={s.header}>
-          <NavLink className={s.headerName} to="/">Home</NavLink>
-          <ul className={s.navLinks}>  
-            <li className={s.navItem}><NavLink className={buildLinkClass} to="/contacts">Contacts</NavLink></li>  
-            <li className={s.navItems}><NavLink className={buildLinkClass} to="/register">Register</NavLink></li>  
-            <li className={s.navItems}><NavLink className={buildLinkClass} to="/login">Login</NavLink></li>  
-          
-        </ul>
-    </div>
+ <div className={s.header}>
+  <div className={s.left}>
+    <NavLink className={s.headerName} to="/">Home</NavLink>
+  </div>
+      {!isLoggedIn && (
+        <div className={s.center}>
+          <p>Hello, plz login or register</p>
+        </div>
+      )}
+      {isLoggedIn && <div className={s.center}>
+        <p>Welcome, {user.name}</p>
+        </div> }
+  <div className={s.right}>
+    <li className={s.navContacts}>
+      <NavLink className={buildLinkClass} to="/contacts">Contacts</NavLink>
+    </li>
+
+    {!isLoggedIn && (
+      <ul className={s.navLinks}>
+        <li><NavLink className={buildLinkClass} to="/register">Register</NavLink></li>
+        <li><NavLink className={buildLinkClass} to="/login">Login</NavLink></li>
+      </ul>
+    )}
+    
+    {isLoggedIn && <button className={s.btn}>Logout</button>}
+  </div>
+</div>
   )
 }
 
